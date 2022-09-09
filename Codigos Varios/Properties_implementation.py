9d7c2d9d5a0ad05c31ada031f00eefdeca2f912b
Federico Aranda <arandafederico10@gmail.com>
2022-09-09 17:11:51 -0300

    Feat/FdA/especific_database_reading
    Begginings of the implementation of an actively modifiable database

--- Codigos Varios/Properties_implementation.py
+++ Codigos Varios/Properties_implementation.py
@@ -26,15 +26,51 @@ cola = queue.LifoQueue()
 
 p = pa.PyAudio()
 ##### Funcion creadora: Pide la entrada de datos para la clase asignadora ####
-def creadora(): #1
-    global freqmic,freqcap,valamp,nombre
-    nombre=str(input("Nombre del material a cargar? :  "))
-    freqmic=newfreq
-    freqcap=capacitivo
-    valamp = amplitud
-    print("Valores asignados, clase creada ")
-    print(nombre,freqmic,freqcap,valamp)
-    return(nombre,freqmic,freqcap,valamp)
+def lectora():
+    with open(csv_file, newline="") as File:
+        print("inicio de lectura")
+        lectura = csv.reader(File)
+        global tmateriales
+        tmateriales = []
+        try:
+            for row in lectura:
+                print(row[0])
+                tmateriales.append(str(row[0]))
+        except:
+            print("final de lista")
+            print("tipos de materiales: ",tmateriales)
+        return tmateriales
+def lectora_de_datos(preg):
+    with open(csv_file, newline="") as File:
+        print("inicio de lectura especifica")
+        lectura = csv.reader(File)
+        for row in lectura:
+            if row[0] == preg:
+                return row
+            else: pass
+
+
+# def creadora(tlista): #1
+#     print(tlista)
+#     global freqmic,freqcap,valamp,nombre
+#     nombre=str(input("Nombre del material a cargar? :  "))
+#     if nombre in tlista:
+#         while True:
+#             preg = input("El material ya existe, asignar nuevos valores? : (S/N)")
+#             preg.lower()
+#             if preg == "s":
+#                 #leer valores existentes y promediar
+#                 freqmic = newfreq
+#                 freqcap = capacitivo
+#                 valamp = amplitud
+#                 break
+#             if preg == "n":
+#                 print("Accion rechazada.. ")
+#                 break
+#             else:
+#                 print("Entrada no valida ")
+#     print("Valores asignados, clase creada ")
+    #return(nombre,freqmic,freqcap,valamp)
 ##### Funcion asignadora: guarda los datos en el diccionario de forma ordenada ####
 def asignadora():#2
     global database
@@ -51,13 +87,6 @@ def almacenadora():#3
     except IOError:
         print("I/O error")
 ###### Funcion lectora: Lee los datos directo de la base de datos
-def lectora():
-    with open(csv_file, newline="") as File:
-        print("inicio de lectura")
-        lectura = csv.reader(File)
-        for row in lectura:
-            print(row)
-
 #funcion de calculos fft (magnitud de frecuencia y amplitud)
 def calculadorafft(cola):
     stream = p.open(                                  # Abrimos el canal de audio con los parámeteros de configuración
@@ -96,15 +125,42 @@ def freqasign(delayint,cola):
     print("freq asignadas, calculando datos importantes....")
     pass
     print(newfreq)
-    creadora()
-    asignadora()
-    almacenadora()
+    #creadora()
+    #asignadora()
+    #almacenadora()
     print("accion completada con exito")
 delayint=int(input("Delay__?:  "))
 print(delayint)
+
+def creadora(tlista): #1
+    print(tlista)
+    global freqmic,freqcap,valamp,nombre
+    nombre=str(input("Nombre del material a cargar? :  "))
+    if nombre in tlista:
+        while True:
+            preg = input("El material ya existe, asignar nuevos valores? : (S/N)")
+            preg.lower()
+            if preg == "s":
+                lecesp = lectora_de_datos(preg)
+                freqmic2 = (lecesp[1] + newfreq)
+                print("intento de valores promediados")
+                freqmic = newfreq
+                freqcap = capacitivo
+                valamp = amplitud
+                break
+            if preg == "n":
+                print("Accion rechazada.. ")
+                break
+            else:
+                print("Entrada no valida ")
+    print("Valores asignados, clase creada ")
+
 # Designa los diferentes threads
 fft_c = threading.Thread(target=calculadorafft, args=(cola,))
 asignar = threading.Thread(target=freqasign, args=(delayint,cola,))
 # inicia los threads
 fft_c.start()
 asignar.start()
+tlista = lectora()
+print(tlista)
+creadora(tlista)
