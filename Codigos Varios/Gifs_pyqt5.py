87625c4e064d47f7cac5fe1600a6b6e06cdd718c
Camilisro <104226156+Camilisro@users.noreply.github.com>
2022-10-18 16:21:43 -0300

    loading gif

--- Codigos Varios/Gifs_pyqt5.py
+++ Codigos Varios/Gifs_pyqt5.py
@@ -2,6 +2,62 @@ import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtGui import QMovie
 from PyQt5.QtCore import Qt
+
+
+class LoadingGif(object):
+
+	def mainUI(self, FrontWindow):
+		FrontWindow.setObjectName("FTwindow")
+		FrontWindow.resize(1024,600)
+		self.centralwidget = QtWidgets.QWidget(FrontWindow)
+		self.centralwidget.setObjectName("main-widget")
+
+		# Label Create
+		self.label = QtWidgets.QLabel(self.centralwidget)
+		self.label.setGeometry(QtCore.QRect(25,25,1024,600))
+		self.label.setMinimumSize(QtCore.QSize(1024,600))
+		self.label.setMaximumSize(QtCore.QSize(1024,600))
+		self.label.setObjectName("lb1")
+		FrontWindow.setCentralWidget(self.centralwidget)
+
+		# Loading the GIF
+		self.movie = QMovie("loader.gif")
+		self.label.setMovie(self.movie)
+
+		self.startAnimation()
+
+	# Start Animation
+
+	def startAnimation(self):
+		self.movie.start()
+
+	# Stop Animation(According to need)
+	#def stopAnimation(self):
+		#self.movie.stop()
+
+
+app = QtWidgets.QApplication(sys.argv)
+window = QtWidgets.QMainWindow()
+demo = LoadingGif()
+demo.mainUI(window)
+window.show()
+sys.exit(app.exec_())
+
+
+"""
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(window)
+    window.show()
+    sys.exit(app.exec_())"""
+#----------------------------------
+"""import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtGui import QMovie
+from PyQt5.QtCore import Qt
 from time import sleep
 class StandbyGif(object):
     def mainUI(self, FrontWindow):
@@ -16,7 +72,7 @@ class StandbyGif(object):
         self.label.setObjectName("lb1")
         FrontWindow.setCentralWidget(self.centralwidget)
 
-        self.movie = QMovie("standby.gif")
+        self.movie = QMovie("GIF-CARGANDO.gif")
         self.label.setMovie(self.movie)
 
         self.startAnimation()
@@ -39,8 +95,8 @@ class LoadingGif(object):
 		self.label.setMaximumSize(QtCore.QSize(1024, 600))
 		self.label.setObjectName("lb1")
 		FrontWindow.setCentralWidget(self.centralwidget)
-		# Loading the GIF
-		self.movie = QMovie(" ")
+		# GIF tirar basura
+		self.movie = QMovie("gif-tirar-basura.gif")
 		self.label.setMovie(self.movie)
 
 		self.startAnimation()
@@ -53,16 +109,16 @@ class LoadingGif(object):
 	# Stop Animation(According to need)
 	def stopAnimation(self):
 		self.movie.stop()
-while True:
-    resp = str(input("Gif a mostrar? "))
+#while True:
+    #resp = str(input("Gif a mostrar? "))
     # correr en multi threading requiero de la funcion de sys.exit(app.exec_()) cada vez que quiero ejecutar un gif
-    app = QtWidgets.QApplication(sys.argv)
-    window = QtWidgets.QMainWindow()
-    demo = LoadingGif()
-    demo2 = StandbyGif()
-    demo.mainUI(window)
-    demo.startAnimation()
-    window.show()
-    demo2.mainUI(window)
-    window.show()
-    sys.exit(app.exec_())
+    #app = QtWidgets.QApplication(sys.argv)
+    #window = QtWidgets.QMainWindow()
+    #demo = LoadingGif()
+    #demo2 = StandbyGif()
+    #demo.mainUI(window)
+    #demo.startAnimation()
+    #window.show()
+    #demo2.mainUI(window)
+    #window.show()
+    #sys.exit(app.exec_())"""
