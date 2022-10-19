198ae251924c43986ff7a5f60365ec735bf3d933
Camilisro <104226156+Camilisro@users.noreply.github.com>
2022-10-19 09:59:22 -0300

    standby gif

--- Codigos Varios/Gifs_pyqt5.py
+++ Codigos Varios/Gifs_pyqt5.py
@@ -2,10 +2,10 @@ import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtGui import QMovie
 from PyQt5.QtCore import Qt
+import time
 
 
 class LoadingGif(object):
-
 	def mainUI(self, FrontWindow):
 		FrontWindow.setObjectName("FTwindow")
 		FrontWindow.resize(1024,600)
@@ -21,19 +21,40 @@ class LoadingGif(object):
 		FrontWindow.setCentralWidget(self.centralwidget)
 
 		# Loading the GIF
-		self.movie = QMovie("loader.gif")
+		self.movie = QMovie("GIF-CARGANDO.gif")
 		self.label.setMovie(self.movie)
 
-		self.startAnimation()
+		#standby Gif
+		#self.movie = QMovie("gif-tirar-basura.gif")
+		#self.label.setMovie(self.Movie)
 
-	# Start Animation
+		#self.startAnimation()
 
-	def startAnimation(self):
+		# Start Animation
+
+		#def startAnimation(self):
 		self.movie.start()
 
-	# Stop Animation(According to need)
-	#def stopAnimation(self):
-		#self.movie.stop()
+		# Stop Animation(According to need)
+
+class SatndbyGif(object):
+	def mainUI(self, FrontWindow):
+		FrontWindow.setObjectName("FTwindow")
+		FrontWindow.resize(1024,600)
+		self.centralwidget = QtWidgets.QWidget(FrontWindow)
+		self.centralwidget.setObjectName("main-widget")
+
+		# Label Create
+		self.label = QtWidgets.QLabel(self.centralwidget)
+		self.label.setGeometry(QtCore.QRect(25,25,1024,600))
+		self.label.setMinimumSize(QtCore.QSize(1024,600))
+		self.label.setMaximumSize(QtCore.QSize(1024,600))
+		self.label.setObjectName("lb1")
+		FrontWindow.setCentralWidget(self.centralwidget)
+
+		# Loading the GIF
+		self.movie = QMovie("gif-tirar-basura.gif")
+		self.label.setMovie(self.movie)
 
 
 app = QtWidgets.QApplication(sys.argv)
@@ -44,15 +65,6 @@ window.show()
 sys.exit(app.exec_())
 
 
-"""
-
-if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
-    window = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(window)
-    window.show()
-    sys.exit(app.exec_())"""
 #----------------------------------
 """import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
